78ec07d04182e6478d6241a3dfedae95fba60c23
prosanjeet <73186809+prosanjeet22@users.noreply.github.com>
2023-10-26 16:54:08 +0530

    Express_js_learning

--- admin.js
+++ admin.js
@@ -1,11 +1,13 @@
+const path=require('path');
 const express=require('express');
 const router=express.Router();
+const rootDir=require('../util/path');
 router.get('/addproduct',(req,res,next)=>{
     console.log("we are in secondmiddleWare");
-    res.send('<form action="/product" method="post"><input type="text" name="name"><button type="button">Add Value</button></form>');
-
+    //res.send('<form action="/product" method="post"><input type="text" name="name"><button type="button">Add Value</button></form>');
+res.sendFile(path.join(rootDir,'views','add-product.html'));
 })
-router.use('/product',(req,res,next)=>{
+router.post('/addproduct',(req,res,next)=>{
     console.log(req.body);
     res.redirect('/')
 });
--- shop.js
+++ shop.js
@@ -1,8 +1,12 @@
+
+const path=require('path');
 const express=require('express');
 const router=express.Router();
+const rootDir=require('../util/path');
 router.get('/',(req,res,next)=>{
-    console.log("in onether express module");
-    res.send("<h1>Hay Welcome to express js</h1>")
+   // console.log("in onether express module");
+   // res.send("<h1>Hay Welcome to express js</h1>")
+res.sendFile(path.join(rootDir,'views','shop.html'));
 })
 
 module.exports=router;
